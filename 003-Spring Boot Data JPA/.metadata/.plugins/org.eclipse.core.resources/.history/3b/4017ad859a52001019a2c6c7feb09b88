b68441f213ecaba5644b8c5e11dbd42616571b14
Arman Khan <95305429+iamarmankhan01@users.noreply.github.com>
2025-06-28 13:05:57 +0530

    Delete 003-Spring Boot Data JPA directory

--- 003-Spring Boot Data JPA/.metadata/.plugins/org.eclipse.core.resources/.history/3b/4017ad859a52001019a2c6c7feb09b88
+++
@@ -1,7 +0,0 @@
-package com.app.arman.repo;
-
-import com.app.arman.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Integer> {
-}
--- 003-Spring Boot Data JPA/05SpringBootDataJpaCollectionMappingEx/src/main/java/com/app/arman/repo/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.arman.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.arman.entity.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Integer>{
-
-}
