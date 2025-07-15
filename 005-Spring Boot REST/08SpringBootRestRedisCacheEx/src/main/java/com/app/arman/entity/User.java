ea1b55580272efee18f49eab0c0f046b608c7319
iamarmankhan01 <ak7157101@gmail.com>
2025-07-15 10:55:12 +0530

    rest

---
+++ 005-Spring Boot REST/08SpringBootRestRedisCacheEx/src/main/java/com/app/arman/entity/User.java
@@ -0,0 +1,21 @@
+package com.app.arman.entity;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue
+	private Integer id;
+	private String name;
+	private String role;
+}
